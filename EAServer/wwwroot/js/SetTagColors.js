673dabdacd3ee78b0854166deb2146c6e534f09b
Trina Luong <u1117393@utah.edu>
2023-01-23 10:04:19 -0700

    Changed the permissions in the manifest file and started working on setting the tag colors

---
+++ EAServer/wwwroot/js/SetTagColors.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    "use strict";
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+
+            //sendData();
+            getMasterCategories();
+
+        });
+    };
+
+    function getMasterCategories() {
+        Office.context.mailbox.masterCategories.getAsync(function (asyncResult) {
+            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+                const categories = asyncResult.value;
+                if (categories && categories.length > 0) {
+                    console.log("Master categories:");
+                    console.log(JSON.stringify(categories));
+                } else {
+                    console.log("There are no categories in the master list.");
+                }
+            } else {
+                console.error(asyncResult.error);
+            }
+        });
+    }
+
+})();
